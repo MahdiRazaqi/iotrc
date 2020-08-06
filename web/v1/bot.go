e8f7bc51698eb9510483a630813d269238fbcac2
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2020-08-06 14:47:10 +0430

    delete telegram bot from projects

--- web/v1/bot.go
+++ web/v1/bot.go
@@ -1,26 +1,25 @@
 package v1
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+// import (
+// 	"encoding/json"
+// 	"io/ioutil"
+// 	"log"
 
-	"github.com/MahdiRazaqi/iotrc/bot"
-	"github.com/labstack/echo"
-)
+// 	"github.com/labstack/echo"
+// )
 
-func listener(c echo.Context) error {
-	body, err := ioutil.ReadAll(c.Request().Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
+// func listener(c echo.Context) error {
+// 	body, err := ioutil.ReadAll(c.Request().Body)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
 
-	var u bot.Update
-	if err := json.Unmarshal(body, &u); err != nil {
-		log.Fatalln(err)
-	}
+// 	var u bot.Update
+// 	if err := json.Unmarshal(body, &u); err != nil {
+// 		log.Fatalln(err)
+// 	}
 
-	bot.MessageHandler(*u.Message)
+// 	bot.MessageHandler(*u.Message)
 
-	return nil
-}
+// 	return nil
+// }
