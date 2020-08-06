54f2e211f74c48726bf03a05a7f429e749de3fb7
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2020-08-06 22:23:37 +0430

    set cross-origin for server

--- web/web.go
+++ web/web.go
@@ -1,13 +1,20 @@
 package web
 
 import (
+	"net/http"
+
 	v1 "github.com/MahdiRazaqi/iotrc/web/v1"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Start server
 func Start() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 	v1.Register(e)
 	e.Start(":8080")
 }
